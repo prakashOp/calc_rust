dc60061f2f74ed9a84464692747645c352e23d1a
prakashOp <prakash127001@gmail.com>
2024-12-12 13:39:42 +0530

    added some funtionality like cue , power , square next go be factorial with recursion

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::io;
 
 fn main() {
+    let mut second_num_state: bool = true;
+
     println!("Rust Calculator");
     println!("Addition -> +");
     println!("Minus -> -");
     println!("Product -> *");
     println!("Division -> /");
     println!("Modulus -> %");
-    println!("Operator -> ");
+    println!("Square -> s");
+    println!("Cube -> c");
+    println!("Power -> p");
+    println!("Factorial -> f");
+    println!("Operator :: ");
 
     let mut user_in_operator = String::new();
 
@@ -19,11 +25,11 @@ fn main() {
         Ok(num) => num,
         Err(_) => {
             println!("InValid Input operator");
-            return; 
+            return;
         }
     };
 
-    println!("Enter First Number");
+    println!("Enter First Number :: ");
 
     let mut first_num = String::new();
 
@@ -39,23 +45,30 @@ fn main() {
         }
     };
 
-    println!("Enter Second Number");
+    println!("Enter Second Number :: ");
 
     let mut second_num = String::new();
 
-    io::stdin()
-        .read_line(&mut second_num)
-        .expect("Failed to Read second number");
+    if user_in_operator == 's' || user_in_operator == 'c' {
+        second_num_state = false;
+    }
 
-    let second_num: f64 = match second_num.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("InValid Input second number");
-            return;
-        }
-    };
+    if second_num_state {
+        io::stdin()
+            .read_line(&mut second_num)
+            .expect("Failed to Read second number");
 
-    calculation(first_num, second_num, user_in_operator);
+        let second_num: f64 = match second_num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("InValid Input second number");
+                return;
+            }
+        };
+        calculation(first_num, second_num, user_in_operator);
+    } else {
+        calculation(first_num, 0.0, user_in_operator);
+    }
 }
 
 fn calculation(first_num: f64, second_num: f64, user_in_operator: char) {
@@ -66,6 +79,9 @@ fn calculation(first_num: f64, second_num: f64, user_in_operator: char) {
         '*' => result = first_num * second_num,
         '/' => result = first_num / second_num,
         '%' => result = first_num % second_num,
+        's' => result = first_num * first_num,
+        'c' => result = first_num * first_num * first_num,
+        'p' => result = first_num.powf(second_num),
         _ => println!("Wrong Operator"),
     }
     println!("Result -> {}", result);
